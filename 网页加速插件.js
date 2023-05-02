7ef70d4d30dab49e558136b71a77d350f6265096
dmqj123 <128874859+dmqj123@users.noreply.github.com>
2023-05-02 10:15:28 +0800

    WebAcc
    详见简介，请在油猴插件内安装

--- 网页加速插件.js
+++ 网页加速插件.js
@@ -2,7 +2,7 @@
 // @name         网页加速器
 // @namespace    http://tampermonkey-delightful
 // @version      0.6
-// @description  加速当前网页的加载速度，并过滤掉带有“广告”关键词的图片
+// @description  加速当前网页的加载速度，并过滤掉广告
 // @include      https://*
 // @include      http://*
 // @grant        GM_addStyle
@@ -11,16 +11,16 @@
 (function() {
     'use strict';
 
-    // 检查是否包含“广告”关键词
-    var containsAds = function(src) {
-        return src.indexOf('广告') >= 0;
+    // 检查是否包含特定关键词或图片源
+    var containsFilterWordsOrSrc = function(src) {
+        return (src.indexOf('广告') >= 0) || (src.indexOf('AD') >= 0) || (src.indexOf('ad') >= 0) || (src.indexOf('https://wan.baidu.com/') || (src.indexOf('https://code.51.com/') === 0);
     }
 
     // 只加速非 Google 搜索页面
     if (window.location.href.indexOf('google') === -1) {
         // 处理图片
         var replaceImages = function(imgElement) {
-            if (!containsAds(imgElement.dataset.src)) {
+            if (!containsFilterWordsOrSrc(imgElement.dataset.src)) {
                 imgElement.onload = null;
                 imgElement.src = imgElement.dataset.src;
             }
